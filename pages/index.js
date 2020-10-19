4fb093f21121f79fe9f2460ae1d6eeab0f575e05
sprkzoff <sprkzoff@gmail.com>
2020-10-20 05:46:53 +0700

    add main crypto price

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,67 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Button } from '@material-ui/core'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Clock from 'react-live-clock'
+
+const url = 'http://localhost:3000/api/';
 
 export default function Home() {
+  const [eth, setEth] = useState(0);
+  const [btc, setBtc] = useState(0);
+  const [omg, setOmg] = useState(0);
+  useEffect(()=>{
+    async function loadData() {
+      const res_eth = await axios.post(url+'yhfinance/quote',{
+        "symbol": "ETHUSD=X",
+        "modules": "price"
+      })
+      //console.log(res_eth.data)
+      setEth(res_eth.data.price.regularMarketPrice)
+      const res_btc = await axios.post(url+'yhfinance/quote',{
+        "symbol": "BTCUSD=X",
+        "modules": "price"
+      })
+      
+      //console.log(res_btc.data)
+      setBtc(res_btc.data.price.regularMarketPrice)
+      const res_omg = await axios.post(url+'yhfinance/quote',{
+        "symbol": "OMG-USD",
+        "modules": "price"
+      })
+      //console.log(res_omg.data)
+      setOmg(res_omg.data.price.regularMarketPrice)
+    }
+    const interval = setInterval(async() => {
+      await loadData();
+    }, 20000);
+    return () => clearInterval(interval);
+  },[]);
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Tradar</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <div className={styles.headBlock}>
+        <h1 className={styles.title}>Tradar</h1>
+      </div>
+      <Clock
+          format={'HH:mm:ss'}
+          ticking={true}
+          timezone={'Asia/Jakarta'}
+      />
+      <div className={styles.grid}>
+        <div className={styles.card}><h3>Bitcoin</h3>${btc}</div>
+        <div className={styles.card}><h3>Ethereum</h3>${eth}</div>
+        <div className={styles.card}><h3>Omise Network</h3>${omg}</div>
+      </div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
       </footer>
     </div>
   )
