9ad6a4bbc29f45e3dedcd2c8b50dc98aa855c445
sprkzoff <sprkzoff@gmail.com>
2020-10-25 07:36:30 +0700

    refactor some duplicate code

---
+++ pages/api/yhfinance/allPrice.js
@@ -0,0 +1,26 @@
+var yahooFinance = require('yahoo-finance');
+
+export default async (req, res) => {
+    let all_crypto = req.body.allSymbols.trim().split(',')
+    let data = []
+    for(let coin of all_crypto)
+    {
+        await yahooFinance.quote({
+            symbol: coin,
+            modules: ["price"]
+        }, function (err, quotes) {
+            //console.log(quotes)
+            data.push(quotes)
+        });
+    }
+      
+    Promise.all(data)
+    .then(() => {
+        console.log('All actions run without any issues!');
+        res.statusCode = 200
+        res.json({'data': data})
+    }).catch((error) => {
+        console.log('An error occured!');
+        console.error(error);
+    });
+}
--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,73 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { Button } from '@material-ui/core'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import Clock from 'react-live-clock'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Button } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Clock from "react-live-clock";
 
-const url = 'http://localhost:3000/api/';
+const url = "http://localhost:3000/api/";
+const allSymbols = "BTCUSD=X,ETHUSD=X,OMG-USD";
 
 export default function Home() {
-  const [eth, setEth] = useState(0);
-  const [btc, setBtc] = useState(0);
-  const [omg, setOmg] = useState(0);
-  const [btc_his, setBtc_his] = useState([]);
-  const [eth_his, setEth_his] = useState([]);
-  const [omg_his, setOmg_his] = useState([]);
+  const [all_crypto, setAll_crypto] = useState([]);
   const [today, setToday] = useState(new Date().toString());
-  useEffect(()=>{
+  useEffect(() => {
     async function loadData() {
-      const res_eth = await axios.post(url+'yhfinance/quote',{
-        "symbol": "ETHUSD=X",
-        "modules": "price"
-      })
-      //console.log(res_eth.data)
-      setEth(res_eth.data.price.regularMarketPrice)
-      const res_btc = await axios.post(url+'yhfinance/quote',{
-        "symbol": "BTCUSD=X",
-        "modules": "price"
-      })
-      
-      //console.log(res_btc.data)
-      setBtc(res_btc.data.price.regularMarketPrice)
-      const res_omg = await axios.post(url+'yhfinance/quote',{
-        "symbol": "OMG-USD",
-        "modules": "price"
-      })
-      //console.log(res_omg.data)
-      setOmg(res_omg.data.price.regularMarketPrice)
+      const res = await axios.post(url + "yhfinance/allPrice", {
+        allSymbols: allSymbols,
+      });
+      let all_price_only = res.data.data.map((item) => {
+        return {
+          name: item.price.shortName,
+          value: item.price.regularMarketPrice,
+        };
+      });
+      //console.log(res.data.data)
+      setAll_crypto(all_price_only);
     }
+    // init
+    loadData(); 
     // loop fetch
-    const interval = setInterval(async() => {
+    const interval = setInterval(async () => {
       await loadData();
       // check if new tick is arrived
-      let temp_today = new Date()
-      if(temp_today.getHours() === 7 && temp_today.getMinutes() === 0 && temp_today.getSeconds() <= 20) {
-        console.log('new day')
+      let temp_today = new Date();
+      if (
+        temp_today.getHours() === 7 &&
+        temp_today.getMinutes() === 0 &&
+        temp_today.getSeconds() <= 20
+      ) {
+        console.log("new day");
         // set state
-        setToday(temp_today.toString())
-      }
-      else {
+        setToday(temp_today.toString());
+      } else {
         // do nothing
-        console.log('same day')
+        console.log("same day");
       }
     }, 20000);
     return () => clearInterval(interval);
-  },[]);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
-        <title>Tradar</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title> Tradar </title> <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.headBlock}>
-        <h1 className={styles.title}>Tradar</h1>
+        <h1 className={styles.title}> Tradar </h1>
       </div>
-      <Clock
-          format={'HH:mm:ss'}
-          ticking={true}
-          timezone={'Asia/Jakarta'}
-      />
+      <Clock format={"HH:mm:ss"} ticking={true} timezone={"Asia/Jakarta"} />
       <div className={styles.grid}>
-        <div className={styles.card}><h3>Bitcoin</h3>${btc}</div>
-        <div className={styles.card}><h3>Ethereum</h3>${eth}</div>
-        <div className={styles.card}><h3>Omise Network</h3>${omg}</div>
-      </div>
-      <main className={styles.main}>
         
-      </main>
-
-      <footer className={styles.footer}>
-      </footer>
+        {all_crypto.map((item) => {
+          return (
+            <div className={styles.card}>
+              <h3> {item.name} </h3> {item.value}
+            </div>
+          );
+        })}
+      </div>
+      <main className={styles.main}> </main>
+      <footer className={styles.footer}> </footer>
     </div>
-  )
+  );
 }
