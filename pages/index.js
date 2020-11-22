49070a5ab763fa9e1f84b5a040d01b6ea67a0494
sprkzoff <sprkzoff@gmail.com>
2020-11-23 06:42:46 +0700

    :zap: add modal template

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Button } from "@material-ui/core";
+import ButtonBase from '@material-ui/core/ButtonBase';
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Clock from "react-live-clock";
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 
 const url = process.env.NEXT_PUBLIC_BACKEND_ENDPOINT;
 const allSymbols = "BTC-USD,ETH-USD,OMG-USD";
@@ -11,7 +15,18 @@ const allSymbols = "BTC-USD,ETH-USD,OMG-USD";
 export default function Home() {
   const [all_crypto, setAll_crypto] = useState([]);
   const [today, setToday] = useState(new Date().toString());
-  const [portfolio,setPortfolio] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+
+  const [selectedSymbol,setSelectedModal] = useState("");
+  const [open, setOpen] = useState(false);
+  const handleOpen = (symbol) => {
+    setOpen(true);
+    setSelectedModal(symbol);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   useEffect(() => {
     // crypto data
     async function loadData() {
@@ -58,19 +73,43 @@ export default function Home() {
         <title> Tradar </title> <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.headBlock}>
-        <h1 className={styles.title}> Tradar </h1>
+        <h1 className={styles.title}> Tradar<TrendingUpIcon style={{ fontSize: 48 }} color="green" /></h1>
       </div>
       <Clock format={"HH:mm:ss"} ticking={true} timezone={"Asia/Jakarta"} />
       <div className={styles.grid}>
         
         {all_crypto.map((item) => {
           return (
-            <div className={styles.card}>
-              <h3> {item.name} </h3> {item.value}
-            </div>
+            <ButtonBase
+              className={styles.cardAction}
+              onClick={() => { handleOpen(item.name) } }
+            >
+              <div className={styles.card}>
+                <h3> {item.name} </h3> {item.value}
+              </div>
+            </ButtonBase>
           );
         })}
       </div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={styles.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={styles.modalpaper}>
+            <h2 id="transition-modal-title">{selectedSymbol}</h2>
+            <p id="transition-modal-description">Add metrices later</p>
+          </div>
+        </Fade>
+      </Modal>
       <main className={styles.main}> </main>
       <footer className={styles.footer}> </footer>
     </div>
